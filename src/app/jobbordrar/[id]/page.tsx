5e22d359fde360894042e3d7d61c763569cd1732
extramers <Samuel.andersson000@gmail.com>
2026-05-19 16:24:27 +0200

    Update page.tsx

--- src/app/jobbordrar/[id]/page.tsx
+++ src/app/jobbordrar/[id]/page.tsx
@@ -9,7 +9,7 @@ export default async function OrderDetail({ params }: { params: any }) {
 
   return (
     <main className="min-h-screen p-4 md:p-8 bg-black text-white print:bg-white print:text-black">
-      <OrderTabs order={order} />
+      <OrderTabs order={order as any} />
     </main>
   );
 }
